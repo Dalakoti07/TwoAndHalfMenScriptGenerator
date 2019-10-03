fb3e806552e90ed2f3ebdf33831c3f3b967b5100
LoneWolf <saurabhdalakoti07@gmail.com>
2019-10-04 00:41:25 +0530

    data collection and new line inserted

--- getTwoAndHalfMenScripts.py
+++ getTwoAndHalfMenScripts.py
@@ -8,25 +8,26 @@ regex = re.compile('^scrolling-script-container')
 
 def getScriptsInList():
 	content = []
-	for i in range(1):# number of seasons
-		for j in range(1): # episodes from 1 to 9
+	for i in range(3):# number of seasons
+		for j in range(9): # episodes from 1 to 9
 			try:
 			    page = urllib.request.urlopen(url+"s0"+str(i+1)+"e"+"0"+str(j+1))
 			except:
 			    print("An error occured.")
 
 			soup = BeautifulSoup(page, 'html.parser')
+			for br in soup.find_all("br"):
+			    br.replace_with("\n")
 			content_lis = soup.find_all('div', attrs={'class': regex})
 			for li in content_lis:
 			    content.append(li.getText())
 			print("episode number "+str(j+1)+" of season "+str(i+1)+" done.")
-	return content
-'''
+
 		for k in range(9,24): # episodes from 9 to 24
 			try:
-			    page = urllib.request.urlopen(url+"s0"+str(i+1)+"e"+"0"+k+1)
+			    page = urllib.request.urlopen(url+"s0"+str(i+1)+"e"+str(k+1))
 			except:
-			    print("An error occured.")
+			    print("An error occured. and page is ",url+"s0"+str(i+1)+"e"+str(k+1))
 
 			soup = BeautifulSoup(page, 'html.parser')
 			content_lis = soup.find_all('div', attrs={'class': regex})
@@ -35,7 +36,7 @@ def getScriptsInList():
 			print("episode number "+str(k+1)+" of season "+str(i+1)+" done.")
 
 	return content
-'''
+
 def writeInFile(fileName,contents):
 	file=open(fileName,"w")
 	# print(type(content[0]))
